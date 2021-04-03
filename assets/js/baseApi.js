6c4d96f1656e427eca5c7069a6326685994d6277
l-dddui <lzekai@foxmail.com>
2021-04-03 16:32:55 +0800

    主页功能完成

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -3,7 +3,22 @@ $.ajaxPrefilter(function (options) {
 
     options.url = baseUrrl + options.url
 
+    if (options.url.indexOf("/my/") != -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    options.complete = function (res) {
+        // console.log(res.responseJSON);
 
+        let obj = res.responseJSON
+        if (obj.status == 1 && obj.message == "身份认证失败！") {
+            // 销毁 token
+            localStorage.removeItem('token')
+            // 跳转到首页
+            location.href = '/login.html'
+        }
+    }
 
 
 })
---
+++ assets/js/index.js
@@ -0,0 +1,56 @@
+$(function () {
+    // 获取用户信息
+    getUserInfo()
+    // 退出登录
+    let layer = layui.layer
+    $('#btn_logout').on('click', function () {
+        layer.confirm('确定退出？', { icon: 3, title: '提示' }, function (index) {
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+            layer.close(index);
+        });
+
+
+
+
+
+    })
+
+
+
+
+
+
+})
+// 获取用户信息
+function getUserInfo() {
+    $.ajax({
+        method: 'get',
+        url: '/my/userinfo',
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
+        success: (res) => {
+            if (res.status != 0) {
+                return alert(res.message)
+            }
+            // console.log(res);
+            renderAvatar(res.data)
+        }
+    })
+}
+
+
+// 渲染头像
+function renderAvatar(user) {
+    let name = user.nickname || user.username
+    $('.welcome').html('欢迎&nbsp;&nbsp;' + name)
+    if (user.user_pic) {
+        $('.layui-nav-img').show().attr('src', user.user_pic)
+        $('.text-avatar').hide()
+    } else {
+        $('.layui-nav-img').hide()
+        let text = name[0].toUpperCase()
+        $('.text-avatar').show().html(text)
+    }
+}
